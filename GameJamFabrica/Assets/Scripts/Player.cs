d5fa906426eb8f5b59f59e3cf3dc94f549d60f7d
Breno Dória <brenord@hotmail.com.br>
2018-10-06 18:50:04 -0300

    [ADD] Health Bar System

---
+++ GameJamFabrica/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    private Stats health;
+
+    private void Awake()
+    {
+        health.Initialize();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            health.CurrentVal -= 10;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            health.CurrentVal += 10;
+        }
+    }
+}
